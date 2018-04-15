6f4c7d35f04826bf4b36af27ff676dcd45534024
Cesar Salinas <cesarsv93@gmail.com>
2018-04-15 14:37:55 -0500

    add state to SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 //From functional-based component to class-based component
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() { //Always have to define a render method
-    return <input />;
+    return (
+      <div>
+        <input onChange={e => this.setState({ term: e.target.value })} />
+      </div>
+    ) 
   }
 }
 
 export default SearchBar;
+
+
+//Normal way
+
+// render() { //Always have to define a render method
+//   return <input onChange={this.onInputChange}/>;
+// }
+
+// onInputChange(e) {
+//   console.log(event.target.value);
+// }
