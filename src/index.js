670a8a23140fa8358900a039b219e1c84129cbc1
Cesar Salinas <cesarsv93@gmail.com>
2018-04-15 18:18:13 -0500

    Add video selection

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyB4Hw2M1G1kGOlkgxo1rO5MG9cIqDIpA5A';
-
-
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null 
+    };
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos });
+      this.setState({ 
+        videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -22,6 +28,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
